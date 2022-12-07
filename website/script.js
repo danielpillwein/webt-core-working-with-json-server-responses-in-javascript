abd816f8d3c31216ccb3b8487c0d352b5b035a14
pillwein <9132tl.rennweg.at>
2022-12-07 13:12:22 +0100

    user-story 3 fast fertig (styling fehlt)

--- website/script.js
+++ website/script.js
@@ -1,4 +1,4 @@
-async function fetchData() {
+async function user_story_1() {
     let response = await fetch("https://jsonplaceholder.typicode.com/todos/1");
 
     if (response.ok) {
@@ -14,4 +14,27 @@ async function fetchData() {
     }
 }
 
-fetchData().then(r => document.body.insertAdjacentElement("afterbegin", r));
+function user_story_2(){
+    fetch("http://localhost/website/data/OpenJsonData_wifi.json")
+        .then(response => response.json())
+        .then(data => console.log(data));
+}
+function user_story_3(){
+    fetch("http://localhost/website/data/OpenJsonData_wifi.json")
+        .then(response => response.json())
+        .then(data => {
+            random = Math.floor(Math.random()*data.length)
+            console.log(data[random])
+            document.getElementById("title").innerHTML = data[random].Titel
+            document.getElementById("province").innerHTML = data[random].Province
+            document.getElementById("place").innerHTML = data[random].Place
+            document.getElementById("description").innerHTML = data[random].Description
+            url = data[random].Url
+            document.getElementById("link").setAttribute("href",url)
+            document.getElementById("link").innerHTML = "weitere Infos"
+            
+        });
+}
+
+//user_story_2()
+//user_story_1().then(r => document.body.insertAdjacentElement("afterbegin", r));
